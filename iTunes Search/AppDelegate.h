9756b7757b634c5f044822a3b448de9450507762
Abhishek Tyagi <abhishektyagi@softwares-imac.local>
2016-11-17 18:51:10 -0500

    Initial Commit

---
+++ iTunes Search/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTunes Search
+//
+//  Created by Abhishek Tyagi on 11/17/16.
+//  Copyright © 2016 Department of Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTunes Search/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTunes Search
+//
+//  Created by Abhishek Tyagi on 11/17/16.
+//  Copyright © 2016 Department of Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
